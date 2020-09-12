40cda788071578b0efde99fc967e69610c0e2657
Tabatha Roman <romantabatha17@gmail.com>
2020-09-12 23:56:41 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,7 +13,7 @@ end
 
 def age_appropriate_birthday(birthday_kids)
  if age !> 12
-   puts 
+ 
    birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
    end
